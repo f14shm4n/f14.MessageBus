a3a77dc903669a11e6776cd2e2e8d90475b8e877
f14shm4n <uraborg@gmail.com>
2025-08-30 21:28:35 +0300

    refactor: changed access level for InMemoryRabbitMQSubscriptionsManager to public.

---
+++ Shared.RabbitMQ/DefaultInMemoryRabbitMQSubscriptionsManager.cs
@@ -0,0 +1,16 @@
+﻿using Shared.EventBus;
+
+namespace Shared.RabbitMQ
+{
+    /// <summary>
+    /// Provides the default implementation of the <see cref="InMemoryEventBusSubscriptionsManager{T}"/> and <see cref="IRabbitMQSubscriptionsManager{T}"/>
+    /// with <see cref="ISubscriptionInfo"/> as <see cref="SubscriptionInfo"/>.
+    /// </summary>
+    public sealed class DefaultInMemoryRabbitMQSubscriptionsManager : InMemoryEventBusSubscriptionsManager<SubscriptionInfo>, IRabbitMQSubscriptionsManager<SubscriptionInfo>
+    {
+        protected override SubscriptionInfo CreateSubscriptionInfo(Type eventType, Type eventHandlerType)
+        {
+            return new SubscriptionInfo(eventHandlerType);
+        }
+    }
+}
