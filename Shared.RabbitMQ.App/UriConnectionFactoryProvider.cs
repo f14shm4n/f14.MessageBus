95856bb3bafaec9b086eeb961dfacd60d1cf249b
f14shm4n <uraborg@gmail.com>
2025-09-01 21:14:37 +0300

    refactor: RabbitMQPersistentConnection is no more abstract class, ConfigureConnectionFactory method replaced by provider.

---
+++ Shared.RabbitMQ.App/UriConnectionFactoryProvider.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+
+namespace Shared.RabbitMQ.App
+{
+    internal sealed class UriConnectionFactoryProvider : IConnectionFactoryProvider
+    {
+        private readonly IConnectionFactory _factory;
+
+        public UriConnectionFactoryProvider(RabbitMQOptions options)
+        {
+            _factory = new ConnectionFactory
+            {
+                Uri = new Uri(options.ConnectionString)
+            };
+        }
+
+        public IConnectionFactory GetConnectionFactory() => _factory;
+    }
+}
