4ee8b8664d7949a5ec157893d0faea4248d7a1cf
f14shm4n <uraborg@gmail.com>
2025-09-02 16:50:32 +0300

    refactor: RabbitMQ/RabbitMQIntegrationTests decomposed to base class.

---
+++ Tests/Tests.RabbitMQ/RabbitMQIntegrationTestBase.cs
@@ -0,0 +1,75 @@
+﻿using DotNet.Testcontainers.Builders;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RabbitMQ.Client;
+using Shared.RabbitMQ;
+using Shared.RabbitMQ.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.RabbitMq;
+
+namespace Tests.RabbitMQ
+{
+    public class RabbitMQIntegrationTestBase : IAsyncLifetime
+    {
+        private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder().Build();
+
+        #region IAsyncLifetime
+
+        public async ValueTask InitializeAsync()
+        {
+            await _rabbitMqContainer.StartAsync().ConfigureAwait(false);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _rabbitMqContainer.DisposeAsync();
+        }
+
+        #endregion
+
+        #region Utility
+
+        protected static ILogger<T> CreateLogger<T>() => Mock.Of<ILogger<T>>();
+
+        protected RabbitMQAppOptions CreateOptions()
+        {
+            return new RabbitMQAppOptions
+            {
+                ConnectionString = _rabbitMqContainer.GetConnectionString(),
+                CalculatorExchange = new Shared.RabbitMQ.RabbitMQExchangeInfo
+                {
+                    Name = "calExchange",
+                    Type = "direct",
+                    Queue = "operations"
+                },
+                ConnectionRetryPolicy = new Shared.RabbitMQ.RabbitMQRetryPolicyOptions
+                {
+                    RetryCount = 1,
+                    RetryDelayInMilliseconds = 500
+                }
+            };
+        }
+
+        protected IConnectionFactoryProvider CreateConnectionFactoryProvider()
+        {
+            var providerMock = new Mock<IConnectionFactoryProvider>();
+            providerMock.Setup(p => p.GetConnectionFactory()).Returns(new ConnectionFactory()
+            {
+                Uri = new Uri(_rabbitMqContainer.GetConnectionString())
+            });
+            return providerMock.Object;
+        }
+
+        protected IRabbitMQPersistentConnection CreateConnection(RabbitMQAppOptions options, IConnectionFactoryProvider? connectionFactoryProvider = null)
+        {
+            connectionFactoryProvider ??= CreateConnectionFactoryProvider();
+            return new RabbitMQPersistentConnection(CreateLogger<RabbitMQPersistentConnection>(), connectionFactoryProvider, options);
+        }   
+
+        #endregion
+    }
+}
--- Tests/Tests.RabbitMQ/RabbitMQIntegrationTests.cs
+++ Tests/Tests.RabbitMQ/RabbitMQIntegrationTests.cs
@@ -1,45 +1,14 @@
-﻿using DotNet.Testcontainers.Builders;
-using FluentAssertions;
-using Microsoft.Extensions.Logging;
-using Moq;
-using RabbitMQ.Client;
-using Shared.RabbitMQ;
-using Shared.RabbitMQ.App;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Testcontainers.RabbitMq;
+﻿using FluentAssertions;
 
 namespace Tests.RabbitMQ
 {
-    public class RabbitMQIntegrationTests : IAsyncLifetime
+    public class RabbitMQIntegrationTests : RabbitMQIntegrationTestBase
     {
-        private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder().Build();
-
-        #region IAsyncLifetime
-
-        public async ValueTask InitializeAsync()
-        {
-            await _rabbitMqContainer.StartAsync().ConfigureAwait(false);
-        }
-
-        public async ValueTask DisposeAsync()
-        {
-            await _rabbitMqContainer.DisposeAsync();
-        }
-
-        #endregion
-
-        #region Tests
-
         [Fact]
         [Trait("DockerPlatform", "Linux")]
         public async Task RabbitMQPersistentConnection_IsConnected()
         {
-            var opts = CreateOptions();
-            await using (var con = new RabbitMQPersistentConnection(Mock.Of<ILogger<RabbitMQPersistentConnection>>(), CreateConnectionFactoryProvider(), opts))
+            await using (var con = CreateConnection(CreateOptions()))
             {
                 var r = await con.TryConnectAsync();
 
@@ -47,40 +16,5 @@ namespace Tests.RabbitMQ
                 con.IsConnected.Should().BeTrue();
             }
         }
-
-        #endregion
-
-        #region Utility
-
-        private RabbitMQAppOptions CreateOptions()
-        {
-            return new RabbitMQAppOptions
-            {
-                ConnectionString = _rabbitMqContainer.GetConnectionString(),
-                CalculatorExchange = new Shared.RabbitMQ.RabbitMQExchangeInfo
-                {
-                    Name = "calExchange",
-                    Type = "direct",
-                    Queue = "operations"
-                },
-                ConnectionRetryPolicy = new Shared.RabbitMQ.RabbitMQRetryPolicyOptions
-                {
-                    RetryCount = 1,
-                    RetryDelayInMilliseconds = 500
-                }
-            };
-        }
-
-        private IConnectionFactoryProvider CreateConnectionFactoryProvider()
-        {
-            var providerMock = new Mock<IConnectionFactoryProvider>();
-            providerMock.Setup(p => p.GetConnectionFactory()).Returns(new ConnectionFactory()
-            {
-                Uri = new Uri(_rabbitMqContainer.GetConnectionString())
-            });
-            return providerMock.Object;
-        }
-
-        #endregion
     }
 }
