3a9caf4f05d1cde7e520ab338b6abfb16b3de955
f14shm4n <uraborg@gmail.com>
2025-09-29 00:04:18 +0300

    modify: added const CalculatorQueueName

--- App.Constants/AppConstants.cs
+++ App.Constants/AppConstants.cs
@@ -5,5 +5,6 @@
         public const string CalculatorExchangeName = "calExchange";
         public const string CalculatorExchangeType = "direct";
         public const bool CalculatorExchangeIsDurable = true;
+        public const string CalculatorQueueName = "operations";
     }
 }
