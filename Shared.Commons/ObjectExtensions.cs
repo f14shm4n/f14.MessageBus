98d3aa5c2c4a690fab634f9e8d661294a6a79e41
f14shm4n <uraborg@gmail.com>
2025-09-29 20:19:16 +0300

    add: added an extensions methods for HashSet and null check for objects.

---
+++ Shared.Commons/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Shared.Commons
+{
+    public static class ObjectExtensions
+    {
+#pragma warning disable CS8777 // Параметр должен иметь значение, отличное от NULL, при выходе.
+        public static bool IsNull<T>([NotNull] this T? o) => o is null;
+        public static bool IsNotNull<T>([NotNull] this T? o) => o is not null;
+#pragma warning restore CS8777 // Параметр должен иметь значение, отличное от NULL, при выходе.
+    }
+}
