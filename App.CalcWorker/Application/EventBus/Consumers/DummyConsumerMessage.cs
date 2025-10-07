dd9445b37d534c51e55e5c4de65baf7a92dd8bb9
f14shm4n <uraborg@gmail.com>
2025-10-07 20:21:38 +0300

    cleanup: removed all unrelated projects.

--- App.CalcWorker/Application/EventBus/Consumers/DummyConsumerMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace App.CalcWorker.Application.EventBus.Consumers
-{
-    internal readonly record struct DummyConsumerMessage
-    {
-        public int Id { get; init; }
-        public string Description { get; init; }
-    }
-}
