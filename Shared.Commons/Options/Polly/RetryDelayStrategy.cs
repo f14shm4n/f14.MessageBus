cd1046e48107b60dea0fc59d191d28888d5b6ef5
f14shm4n <uraborg@gmail.com>
2025-10-07 17:12:01 +0300

    refactor: Shared.Common replaced by f14.Common

--- Shared.Commons/Options/Polly/RetryDelayStrategy.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shared.Commons.Options.Polly
-{
-    public enum RetryDelayStrategy
-    {
-        Linear
-    }
-}
--- Shared.Commons/Options/Polly/RetryPolicyOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Commons.Options.Polly
-{
-    public class RetryPolicyOptions
-    {
-        public int RetryCount { get; set; } = 3;
-        public int RetryDelayInMilliseconds { get; set; } = 3000;
-        public RetryDelayStrategy DelayStrategy { get; set; } = RetryDelayStrategy.Linear;
-    }
-}
